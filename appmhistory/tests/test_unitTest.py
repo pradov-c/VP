b02207a84114ddb030bd94b6840b475162be4e51
veronica prado <veronica.prado@jalasoft.com>
2017-02-12 20:34:44 -0400

    Adding unit test and files to package

---
+++ appmhistory/tests/test_unitTest.py
@@ -0,0 +1,22 @@
+import unittest
+import requests
+import xmlrunner
+
+class TestViews(unittest.TestCase):
+    def setUp(self):
+        self.command = 'curl'
+        self.server = 'http://127.0.0.1'
+        self.port = '8000'
+        self.application_name = '/appmhistory'
+        print('Init test')
+
+    def test_request_POST(self):
+        res = requests.post(self.server + ':' + self.port + self.application_name + '/', headers='', data= {'name':'Maria Perez', 'ci':'451','register_date':'2017-01-13'})
+        self.assertTrue(res.status_code == 201)
+
+    def test_request_GET(self):
+        res = requests.get(self.server + ':' + self.port + self.application_name + '/', headers='')
+        self.assertTrue(res.status_code == 200)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    # Application name:
+    name="MedicalHistory",
+
+    # Version number (initial):
+    version="0.1.0",
+
+    # Application author details:
+    author="veronica prado",
+    author_email="pradovc01@gmail.com",
+
+    # Packages
+    packages=["appmhistory"],
+
+    # Include additional files into the package
+    include_package_data=True,
+
+    #
+    license="LICENSE.txt",
+
+)
