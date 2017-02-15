b64d4ebde6d5005902bc599c75cd367d22f0112f
veronica prado <veronica.prado@jalasoft.com>
2017-02-14 22:52:54 -0400

    adding task on fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -6,25 +6,37 @@ VENV_DIR = os.path.join(BASE_DIR,'venmhistory/bin/activate')
 env.password = 'vagrant'
 env.hosts = ['localhost']
 env.user = 'vagrant'
+env.shell = "/bin/bash"
+VENV_COMMAND = 'source '+ VENV_DIR
 
 @task
 def start():
     """
-         Start app
-         Steps:
          1.Activate virtual env
          2.Run server
+         3.Collecstatic
+         4. createdb sqlLite
     """
     print("Start Application: ")
     venv_command = 'source '+ VENV_DIR
     with cd(BASE_DIR):
-        run('virtualenv --no-site-packages venmhistory --always-copy')
-        run (venv_command)
-    run('python ' +  BASE_DIR + 'manage.py collectstatic --noinput')
-    run('python ' +  BASE_DIR + 'manage.py migrate')
+        local('virtualenv --no-site-packages venmhistory --always-copy', shell=env.shell)
+        local(VENV_COMMAND, shell=env.shell)
+    local('python ' + BASE_DIR + 'manage.py collectstatic --noinput', shell=env.shell)
+    local('python ' +  BASE_DIR + 'manage.py syncdb --noinput', shell=env.shell)
 
 
 @task
 def clean():
     """Cleans Python bytecode"""
     sudo('find . -name \'*.py?\' -exec rm -rf {} \;')
+
+@task
+def runDjangoServer():
+    local(VENV_COMMAND, shell=env.shell)
+    local('python '+ BASE_DIR + 'manage.py runserver', shell=env.shell)
+
+@task
+def runUnitTest():
+    local('python ' + BASE_DIR + 'appmhistory/tests/test_unitTest.py',shell=env.shell)
+
