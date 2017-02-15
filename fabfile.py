6c273ed5a9624aaf702cc1d2bfd0c6c2fe6dae40
veronica prado <veronica.prado@jalasoft.com>
2017-02-14 20:36:22 -0400

    Removing dependencies from fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -18,14 +18,8 @@ def start():
     print("Start Application: ")
     venv_command = 'source '+ VENV_DIR
     with cd(BASE_DIR):
-        print('run virtualenv venmhistory copu')
-        run('sudo virtualenv --no-site-packages venmhistory --always-copy')
+        run('virtualenv --no-site-packages venmhistory --always-copy')
         run (venv_command)
-        run('sudo pip install requests')
-        run('sudo pip install django')
-        run('sudo pip install djangorestframework')
-        run('sudo pip install markdown')
-        run('sudo pip install django-filter')
     run('python ' +  BASE_DIR + 'manage.py collectstatic --noinput')
     run('python ' +  BASE_DIR + 'manage.py migrate')
 
