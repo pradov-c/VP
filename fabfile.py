2b209a03bc9b3d30aae79224873146566581f717
veronica prado <veronica.prado@jalasoft.com>
2017-02-23 23:54:40 -0400

    deploy in remote vm

--- fabfile.py
+++ fabfile.py
@@ -68,7 +68,15 @@ def runUnitTest():
 @task
 def runServer():
     local(VENV_COMMAND, shell=env.shell)
+    local("screen -dm bash -c 'python manage.py runserver; exec sh '",shell=env.shell)
+
+
+@task
+def runRemoteServer(var_remote_server):
+    local(VENV_COMMAND, shell=env.shell)
     local('nohup python ' + BASE_DIR + 'manage.py runserver & ', shell=env.shell)
+
+
 
 @task
 def runAcceptanceTest(var_deploy_folder):
@@ -81,10 +89,10 @@ def packageApplication():
     local('sudo python setup.py sdist')
 
 @task
-def deployApp(var_artifact):
-    local('tar -xzf '+ var_artifact ) #unzip artifact
-
-
+def deployApp(var_artifact,var_remote_user,var_server):
+    local('sshpass -p "'+var_remote_user +'" scp '+var_artifact+' '+var_remote_user+'@'+var_server+':/home/'+var_remote_user+'/'+var_artifact)#copy remote server
+    local('sshpass -p "'+var_remote_user +'" ssh  '+var_remote_user+'@'+var_server+' tar -xzf /home/'+var_remote_user+'/'+var_artifact) #unzip artifact
+    local('sshpass -p "'+var_remote_user +'" scp  fabfile.py '+var_remote_user+'@'+var_server+':/home/'+var_remote_user+'/MedicalHistory-0.1.0')#copy remote server
 
 @task
 def cleanWorkSpace(var_work_space):
